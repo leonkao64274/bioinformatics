28f9389e665e7d3e67c075f0eed1b2944da5ca35
leonkao64274 <61872763+leonkao64274@users.noreply.github.com>
2020-03-06 23:33:43 +0800

    Update ortho.py
    move padding to sequence_processing

--- ortho.py
+++ ortho.py
@@ -28,25 +28,5 @@ def gene2og(x="9606"):
 
 
 
-#----------------------------------------------------------------------
-'''
-padding sequences
-
-'''
-
-def pad_sequence_arrays(seq_arrays, pad_array, num, direction):
-    padded_seq = []
-    for i in seq_arrays:
-        if direction == 'both':
-            padded_seq.append([pad_array]*num + i.tolist() + [pad_array]*num)
-        elif direction == 'pre':
-            padded_seq.append([pad_array]*num + i.tolist())
-        elif direction == 'post':
-            padded_seq.append(i.tolist() + [pad_array]*num)
-        else:
-            print('Wrong Parameter!!')
-    return np.array(padded_seq)
-    
-    
     
   
