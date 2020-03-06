fb028df9175cbc50b8c56c6f52061eb88d49b163
leonkao64274 <61872763+leonkao64274@users.noreply.github.com>
2020-03-06 22:54:23 +0800

    Create ortho.py

---
+++ ortho.py
@@ -0,0 +1,52 @@
+import numpy as np
+#-----------------------------------------
+'''
+get ortho geneID form ortho database
+
+'''
+def load_ortho(x="ortho/rmh_r_geneid.txt"):
+    with open(x,"r") as f:
+        geneids=f.readlines()
+    ortho_geneids=[geneid.rstrip() for geneid in geneids]
+    return ortho_geneids
+#----------------------------------------------------------------------
+'''
+get the ortho_gropuID
+x:taxID
+
+'''
+def gene2og(x="9606"):
+    rdata2=[]
+    with open ("odb10v1_OG2genes.tab","r") as f:
+        rdata2=f.readlines()
+    ogs=[line.split("\t")[0] for line in tqdm(rdata2)]
+    orthoids_2=[line.split("\t")[1] for line in tqdm(rdata2)]
+    ogs_orthoids=[[ogs[i],orthoids_2[i].rstrip()] for i in tqdm(range(len(orthoids_2))) if orthoids_2[i][0:4]==x]
+
+    return ogs_orthoids
+
+
+
+
+#----------------------------------------------------------------------
+'''
+padding sequences
+
+'''
+
+def pad_sequence_arrays(seq_arrays, pad_array, num, direction):
+    padded_seq = []
+    for i in seq_arrays:
+        if direction == 'both':
+            padded_seq.append([pad_array]*num + i.tolist() + [pad_array]*num)
+        elif direction == 'pre':
+            padded_seq.append([pad_array]*num + i.tolist())
+        elif direction == 'post':
+            padded_seq.append(i.tolist() + [pad_array]*num)
+        else:
+            print('Wrong Parameter!!')
+    return np.array(padded_seq)
+    
+    
+    
+  
